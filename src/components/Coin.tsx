2ab53e1b0147069cc9d07544f8ebdfed23657f74
VINUHUB <aladoemeka1@gmail.com>
2026-01-12 04:40:38 -0800

    Update Coin.tsx

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,20 +1,24 @@
-import { FC } from "react";
-
-export type CoinSide = "heads" | "tails";
+import { FC } from 'react';
 
 interface CoinProps {
-  side: CoinSide;
-  isFlipping: boolean;
+  coinSide: "heads" | "tails";
+  flipping: boolean;
 }
 
-export const Coin: FC<CoinProps> = ({ side, isFlipping }) => (
+export const Coin: FC<CoinProps> = ({ coinSide, flipping }) => (
   <div className="coin-container mb-6 sm:mb-8 mx-auto">
-    <div className={`coin ${isFlipping ? "flipping" : side}`}>
+    <div className={`coin ${flipping ? "flipping" : coinSide}`}>
       <div className="side heads">
-        <img src="/heads.png" alt="Heads" />
+        <img 
+          src="https://photos.pinksale.finance/file/pinksale-logo-upload/1760517373672-b81d4b93ee4e5dcd9d7463e941d680c4.png" 
+          alt="Heads" 
+        />
       </div>
       <div className="side tails">
-        <img src="/tails.png" alt="Tails" />
+        <img 
+          src="https://photos.pinksale.finance/file/pinksale-logo-upload/1760517398339-0e799d5e0f9ab9230cd2fc425ea13ac5.png" 
+          alt="Tails" 
+        />
       </div>
     </div>
   </div>
