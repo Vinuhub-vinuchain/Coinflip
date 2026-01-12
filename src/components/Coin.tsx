178541587d3ba02fb7df2191cebc25b85ef40c87
VINUHUB <aladoemeka1@gmail.com>
2026-01-12 06:20:23 -0800

    Update Coin.tsx

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -5,9 +5,9 @@ interface CoinProps {
   flipping: boolean;
 }
 
-export const Coin: FC<CoinProps> = ({ coinSide, flipping }) => (
+export const Coin: FC<CoinProps> = ({ side, flipping }) => (
   <div className="coin-container mb-6 sm:mb-8 mx-auto">
-    <div className={`coin ${flipping ? "flipping" : coinSide}`}>
+    <div className={`coin ${flipping ? "flipping" : side}`}>
       <div className="side heads">
         <img 
           src="https://photos.pinksale.finance/file/pinksale-logo-upload/1760517373672-b81d4b93ee4e5dcd9d7463e941d680c4.png" 
